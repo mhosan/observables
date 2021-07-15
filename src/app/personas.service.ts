f442cf7e0ed2e1c3aab93e329f1d91eef130580d
marcelo <mhosan@gmail.com>
2021-07-15 12:44:43 -0300

    nada

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 
 /*
-Observable: un flujo de datos (stream) en el tiempo. 
+Observable: "un flujo de datos (stream) en el tiempo."
+
 Un subject es un tipo especial de observable que permite realizar varias tareas
 Es especial porque aparte de ser un observable puede ser un observador
+
+El subject es el que genera el observable, es común que terminen en $
+El subject emite con el metodo next()
 
 Un BehaviorSubject extiende al subject y emite el último valor a las nuevas suscripciones
 El BehaviorSubject requiere un valor por defecto (el subject no), devuelve el último valor cuando hay 
 una nueva suscripcion (esto en el subject no ocurre) y permite recuperar el último valor con getValue() 
 
-El subject es el que genera el observable, es común que terminen en $
-El subject emite con el metodo next()
-Para recibir lo que emite con el metodo next() hay que estar suscripto y esta suscripción debe estar hecha antes
+Para recibir lo que se emite con el metodo next() hay que estar suscripto y esta suscripción debe estar hecha antes
 que el metodo next() emita. O sea, primero la suscrip y luego la emision. Por ej esto muestra el observable por consola
+
 this.personas$.subscribe(respuesta => console.log(respuesta));
+
 this.personas$.next('hola mundo');
 pero esto no. Esto muestra a partir de la subscripcion
 this.personas$.next('hola mundo');
@@ -34,7 +38,7 @@ export interface Persona {
 })
 export class PersonasService {
   private personas: Persona[];
-  private personas$: Subject<Persona[]>  //el subject que va a emitir un array de personas
+  private personas$: Subject<Persona[]>  //declaración del subject que va a emitir un array de personas
   constructor() {
     this.personas = [];
     this.personas$ = new Subject();
